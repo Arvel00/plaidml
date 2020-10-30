c26ffa010c374fb86fb6e4592f063156d04f5d9c
Artur Bialas <artur.bialas@intel.com>
2020-10-30 18:00:43 +0100

    vectorize mem initial

--- pmlc/dialect/pxa/transforms/passes.h
+++ pmlc/dialect/pxa/transforms/passes.h
@@ -70,6 +70,8 @@ std::unique_ptr<mlir::Pass> createVectorizePass(mlir::StringRef strategy,
 
 std::unique_ptr<mlir::Pass> createSimplifyWithConstraintsPass();
 
+std::unique_ptr<mlir::Pass> createVectorizeMemPass();
+
 /// Generate the code for registering passes.
 #define GEN_PASS_REGISTRATION
 #include "pmlc/dialect/pxa/transforms/passes.h.inc"
